3fea87f79567d9f98fa04c285b18c0e8fa8ce17f
Derek Leach <mail@derek.name>
2014-09-09 15:45:30 +0200

    Initial commit of Lazyvore web app. This is currently a copy of the Pyramid wiki2 tutorial.

---
+++ setup.py
@@ -0,0 +1,50 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'pyramid',
+    'pyramid_chameleon',
+    'pyramid_debugtoolbar',
+    'pyramid_tm',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+    'waitress',
+    'docutils',
+    'WebTest',
+    'alembic',
+    ]
+
+setup(name='Lazyvore',
+      version='0.1.0',
+      description='Lazyvore',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='Derek Leach',
+      author_email='develop@lazyvo.re',
+      url='lazyvo.re',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='lazyvore',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = lazyvore:main
+      [console_scripts]
+      initialize_Lazyvore_db = lazyvore.scripts.initializedb:main
+      """,
+      )
