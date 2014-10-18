c30e74849edf8b0992a88318885422f6284ceb1d
Derek Leach <mail@derek.name>
2014-10-18 13:55:36 +0200

    Added files to allow app to run in AWS Elastic Beanstalk Docker container.

--- alembic/env.py
+++ alembic/env.py
@@ -2,6 +2,7 @@ from __future__ import with_statement
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
+from os.path import expandvars
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -35,7 +36,7 @@ def run_migrations_offline():
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = expandvars(config.get_main_option("sqlalchemy.url"))
     context.configure(url=url, target_metadata=target_metadata)
 
     with context.begin_transaction():
@@ -48,8 +49,10 @@ def run_migrations_online():
     and associate a connection with the context.
 
     """
+    url = expandvars(config.get_main_option("sqlalchemy.url"))
     engine = engine_from_config(
                 config.get_section(config.config_ini_section),
+                url=url,
                 prefix='sqlalchemy.',
                 poolclass=pool.NullPool)
 
--- lazyvore.wsgi
+++ lazyvore.wsgi
@@ -1,4 +1,6 @@
 from pyramid.paster import get_app, setup_logging
-ini_path = '/home/derek/code/Lazyvore/production.ini'
+from os.path import expandvars
+
+ini_path = expandvars('$INI_PATH')
 setup_logging(ini_path)
 application = get_app(ini_path, 'main')
